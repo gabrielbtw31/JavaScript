4f8b75b9e9e5956cf5896cf32fcdfbeabb527d8f
gabrielbtw31 <60615690+gabrielbtw31@users.noreply.github.com>
2020-04-06 00:37:26 +0200

    Add files via upload

---
+++ Act05.1/js/act05.1.js
@@ -0,0 +1,6 @@
+var tabla = 10;
+var cantidad = 5;
+
+  for(var i=1; i<=cantidad; i++){
+      document.write(tabla+"X"+i+"="+(tabla*i)+"<br>");
+  }
---
+++ Act05/js/act05.js
@@ -0,0 +1,24 @@
+var bucle = 1;
+var cont = 0
+var cont2 = 1;
+
+document.write("<table>");
+
+while(bucle <= 100){
+    if(cont == 0){
+        document.write("<tr>");
+    }
+
+    cont++;
+
+    document.write("<td>"+cont2+" x " +cont+" =" +cont * cont2+"</td>");
+
+    bucle++;
+
+    if(cont ==10){
+        document.write("<td>");
+        cont = 0;
+        cont2++;
+    }
+}
+document.write("</table>");
